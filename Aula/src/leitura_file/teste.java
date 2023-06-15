32bfc5a7c88e5e5105eca52290c971cfda8ec49e
Helton Carneiro <heltoncarneiro@outlook.com>
2023-06-15 12:08:41 -0300

    file ajutes BufferedRead

--- Aula/src/leitura_file/teste.java
+++ Aula/src/leitura_file/teste.java
@@ -1,9 +1,9 @@
 package leitura_file;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
 
 public class teste {
@@ -37,28 +37,16 @@ public class teste {
 		
 		*/
 		
-		
 		String path = "C:\\Users\\helto\\OneDrive\\Documentos\\in.txt";
-		FileReader fr = null;
-		BufferedReader bf = null;
-		try {
-			fr = new FileReader(path); //instanciação do fr a partir local do arquivo
-			bf = new BufferedReader(fr); // instaciação do bf é feita a partir de um FileReader já instaciado anteriomente
-			// bf = new BufferedReader(FileReader(path)); outra maneira de instanciar, no caso diretamente
-			String lerfile = bf.readLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String lerfile = br.readLine();
 			while(lerfile != null) {
 				System.out.println(lerfile);
-				lerfile = bf.readLine();
+				lerfile = br.readLine();
 			}
 		}
 		catch (IOException e) {
 			System.out.println("Erro: "+ e.getMessage());
-		}
-		finally {
-			if(fr != null || bf != null) {
-				bf.close();
-				fr.close();
-			}
 		}
 	}
 
