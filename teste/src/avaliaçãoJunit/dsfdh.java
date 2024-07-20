13a2bf041137f5d82fbf503ab1f02905b1c19124
heltoncarneiro <heltoncarneiro@outlook.com>
2024-07-20 15:44:05 -0300

    update

---
+++ teste/src/avaliaçãoJunit/dsfdh.java
@@ -0,0 +1,14 @@
+package avaliaçãoJunit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class dsfdh {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
